33bf5b94c132668053b9195431d5a83cd57786b6
monicagithub <monicagithub@gmail.com>
2014-05-29 22:40:54 +0200

    commit init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carrito::Application.config.secret_key_base = 'e980537a20c31c4c39a5519d2fd20a4ef2a37ec6cc5238d3340f7cb8d40db4cc7dbc94081fa5bd95d729d16ac818d843bf7d1d04814cec57063748c81e31d561'
---
+++ db/seedsviejo.rb
@@ -0,0 +1,45 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+# . . .
+Product.create!(title: 'Programming Ruby 1.9 & 2.0',
+description:
+<p>
+Ruby is the fastest growing and most exciting dynamic language
+out there. If you need to get working programs delivered fast,
+you should add Ruby to your toolbox.
+</p>,
+image_url: 'ruby.jpg',
+price: 49.95)
+# . . .
+# . . .
+Product.create!(title: 'Memorias de una reina',
+description:
+<p>
+Una historia de terror
+</p>,
+image_url: 'ruby.jpg',
+price: 49.95)
+# . . .
+Product.create!(title: 'El anatomista',
+description:
+<p>
+Una novela maravillosa
+</p>,
+image_url: 'ruby.jpg',
+price: 49.95)
+# . . .
+# . . .
+Product.create!(title: 'Confieso que he vivido',
+description:
+<p>
+Historia de una vida de un genio
+</p>,
+image_url: 'ruby.jpg',
+price: 49.95)
+# . . .
